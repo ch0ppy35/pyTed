29a16be9f697560068d2ab3295ef4a6e96592504
Mike Miller <chop3593@gmail.com>
2020-01-13 20:15:35 -0700

    meh

---
+++ pyTED.py
@@ -0,0 +1,27 @@
+import http.client
+from xml.etree.ElementTree import fromstring, ElementTree
+
+
+def getInfo():
+    conn = http.client.HTTPConnection('IP GOES HERE', 8880, 5)
+    payload = ""
+    headers = {
+        'cache-control': "no-cache",
+    }
+
+    conn.request("GET", "/api/LiveData.xml", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode("utf-8")
+    tree = ElementTree(fromstring(data))
+    root = tree.getroot()
+    for child in root:
+        print(child.tag, child.attrib)
+
+
+def main():
+    getInfo()
+
+
+if __name__ == '__main__':
+    main()
