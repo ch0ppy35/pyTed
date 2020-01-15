f04a4636cccdd51a94d31a42b7229b9b344fcaf8
Mike Miller <chop3593@gmail.com>
2020-01-14 20:29:32 -0700

    flask tweaking

--- app/routes.py
+++ app/routes.py
@@ -3,24 +3,30 @@ from getInfo import getData
 from flask_executor import Executor
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
-import time
 import psycopg2
 import threading
 
 executor = Executor(app)
+
+
 @app.before_first_request
 def startBackGroundJob():
-        threading.Thread(target=activateJob).start()
+    threading.Thread(target=activateJob).start()
+
+
 def activateJob():
     executor.submit(getData())
+
+
 def qryCurrent():
-    conndb = psycopg2.connect(host='HOST', port='PORT', database='pyted', user='USER', password='PASSWORD')
-    sql = "select * from voltage ORDER BY id DESC LIMIT 1;"
+    conndb = psycopg2.connect(host='DBHOST', port='PORT', database='pyted', user='USER', password='PASSWORD')
+    sql = "SELECT * FROM voltage ORDER BY id DESC LIMIT 1;"
     qry = conndb.cursor()
     qry.execute(sql)
     results = (qry.fetchall())
     print(results)
-    return('</br>'.join(str(e) for e in results))
+    return ('</br>'.join(str(e) for e in results))
+
 
 @app.route('/')
 @app.route('/index')
