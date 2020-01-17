957e316ada77653cdcb005d61df93d7451938c3c
JustBeanie <48847633+JustBeanie@users.noreply.github.com>
2020-01-17 14:00:42 -0500

    Front end work & clean up

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     HOST = os.environ.get('HOST')
     DBHOST = os.environ.get('DBHOST')
     DBPORT = os.environ.get('DBPORT')
     DBUSER = os.environ.get('DBUSER')
-    DBPASS = os.environ.get('DBPASS') or 'oh-yeah'
+    DBPASS = os.environ.get('DBPASS')
+    PWS = os.environ.get('PWS') or 'KCALONGB124'
