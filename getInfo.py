6f88ecd147e3c8a3bd13badb3a65e9e635903e52
justbeanie <contact@beaniebot.me>
2020-01-21 12:39:18 -0500

    Use APScheduler for background tasks

--- getInfo.py
+++ getInfo.py
@@ -6,29 +6,27 @@ from app import app, database
 
 
 def getData():
-    while True:
-        data = goget()
-        tree = ElementTree(fromstring(data))
-        root = tree.getroot()
+    data = goget()
+    tree = ElementTree(fromstring(data))
+    root = tree.getroot()
 
-        #   Get Data
-        voltageNow = (float(root.getchildren()[2].getchildren()[0].getchildren()[0].text) / 10)
-        wattsNow = (float(root.getchildren()[3].getchildren()[0].getchildren()[0].text) / 1000)
+    #   Get Data
+    voltageNow = (float(root.getchildren()[2].getchildren()[0].getchildren()[0].text) / 10)
+    wattsNow = (float(root.getchildren()[3].getchildren()[0].getchildren()[0].text) / 1000)
 
-        #   Build & execute Query
-        #   Voltage
-        db = database.MyDatabase()
-        sql = 'INSERT INTO Voltage(voltage) VALUES(%s);' % voltageNow
-        #print(sql)
-        db.insertq(sql)
+    #   Build & execute Query
+    #   Voltage
+    db = database.MyDatabase()
+    sql = 'INSERT INTO Voltage(voltage) VALUES(%s);' % voltageNow
+    # print(sql)
+    db.insertq(sql)
 
-        #   Watts
-        db = database.MyDatabase()
-        sql = 'INSERT INTO killawatts(killawatts) VALUES(%s);' % wattsNow
-        #print(sql)
-        db.insertq(sql)
+    #   Watts
+    db = database.MyDatabase()
+    sql = 'INSERT INTO killawatts(killawatts) VALUES(%s);' % wattsNow
+    # print(sql)
+    db.insertq(sql)
 
-        # print('Updated!')
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Updated the DB with fresh data')
-        time.sleep(30)
+    # print('Updated!')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Updated the DB with fresh data')
