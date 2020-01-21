6cfbc5a8052ed32f0de8a1b7f0ecff2fdfe167f3
justbeanie <contact@beaniebot.me>
2020-01-21 12:39:18 -0500

    Use APScheduler for background tasks

--- app/routes.py
+++ app/routes.py
@@ -2,20 +2,22 @@ from app import app, database
 from getInfo import getData
 from flask_executor import Executor
 from flask import render_template
-import threading
+from apscheduler.schedulers.background import BackgroundScheduler
 import time
 
 executor = Executor(app)
 
 @app.before_first_request
 def startBackGroundJob():
-    threading.Thread(target=activateJob).start()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        getData,
+        trigger='cron',
+        second='*/30'
+    )
+    scheduler.start()
     #give time for new data to be inserted.
     time.sleep(5)
-
-def activateJob():
-    executor.submit(getData())
-
 def qryCurrent():
     sql = """
     SELECT v.voltage, k.killawatts
