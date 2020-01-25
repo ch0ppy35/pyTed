587b2ee31521c55ce128c4c7f9292a1036e424e2
Mike Miller <mmiller@levelsbeyond.com>
2020-01-25 02:36:03 -0700

    -Added custom error handling -Moved core files around -Prevented unhealthy exits -General clean up

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import logging
 import os
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from config import Config
 
 app = Flask(__name__)
@@ -10,9 +10,24 @@ app.config.from_object(Config)
 if not os.path.exists('logs'):
     os.mkdir('logs')
 
+# Normal logging
 file_handler = RotatingFileHandler('logs/pyted.log', maxBytes=10240, backupCount=10)
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 file_handler.setLevel(logging.INFO)
+
+# Mail logging on errors (Needs work)
+if not app.debug and app.config['MAIL_SERVER'] != '':
+    credentials = None
+    app.logger.info('~ Trying to turn on mail services ~')
+    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        credentials = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                               'no-reply@' + app.config['MAIL_SERVER'], app.config['ADMINS'], 'pyTed failure',
+                               credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+else:
+    app.logger.info('~ Not turning on mail services ~')
 
 app.logger.addHandler(file_handler)
 app.logger.setLevel(logging.INFO)
--- config.py
+++ config.py
@@ -4,10 +4,27 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config(object):
+    # TED Endpoint
     HOST = os.environ.get('HOST')
+    # Weather station
+    PWS = os.environ.get('PWS') or 'KCALONGB124'
+    # Database
     DBHOST = os.environ.get('DBHOST')
-    DBPORT = os.environ.get('DBPORT')
+    DBPORT = os.environ.get('DBPORT') or '5432'
     DBUSER = os.environ.get('DBUSER')
     DBPASS = os.environ.get('DBPASS')
-    PWS = os.environ.get('PWS') or 'KCALONGB124'
+    # Mail Server
+    MAIL_SERVER = ''
+    MAIL_PORT = 25
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # Admins
+    ADMINS = ['noreply@null.com']
+    # available languages
+    LANGUAGES = {
+        'en': 'English'
+    }
