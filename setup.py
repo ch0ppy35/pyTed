20eb4e92669e98ea19cce74cdf285b76af623aba
justbeanie <contact@beaniebot.me>
2020-01-27 15:04:29 -0500

    More setup Logic

--- setup.py
+++ setup.py
@@ -1,5 +1,39 @@
-from app import database
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from app import app
+import time
+
+
+def dbCheck():
+    conn = psycopg2.connect(host=app.config['DBHOST'], port=app.config['DBPORT'], database='postgres',
+                                user=app.config['DBUSER'], password=app.config['DBPASS'])
+    sql = """
+    SELECT EXISTS(
+     SELECT datname FROM pg_catalog.pg_database 
+     WHERE LOWER(datname) = LOWER('%(s)s')
+    );
+    """ % {'s': app.config['DBDB']}
+    cur = conn.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()[0][0]
+
+    if not result:
+        app.logger.info('(!) Database does not exist! Running setup!')
+        time.sleep(5)
+        dbSetup()
+    else:
+        app.logger.info('~ pyTed db exists! ~')
+
 
 def dbSetup():
-    # do stuff
-    return 'done!'
+    conn = psycopg2.connect(host=app.config['DBHOST'], port=app.config['DBPORT'], database='postgres',
+                                user=app.config['DBUSER'], password=app.config['DBPASS'])
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    sql = """
+    CREATE DATABASE %(s)s; 
+    """ % {'s': app.config['DBDB']}
+    cur.execute(sql)
+    app.logger.info('Database created!')
+    time.sleep(5)
+
