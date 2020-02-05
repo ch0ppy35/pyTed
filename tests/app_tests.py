3dcbe53cee27120f671ac6205872019bda0fe7f1
Mike Miller <chop3593@gmail.com>
2020-02-05 00:09:57 -0700

    basic tests working!

---
+++ tests/app_tests.py
@@ -0,0 +1,29 @@
+from app import app
+import unittest
+
+class FlaskBookshelfTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_rtkw(self):
+        result = self.app.get('/rtkw')
+        self.assertEqual(result.status_code, 200)
