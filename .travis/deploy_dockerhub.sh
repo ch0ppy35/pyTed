5e8598d43872c5cf44a85ac908abcddb4c332802
Mike Miller <chop3593@gmail.com>
2020-02-11 21:13:11 -0700

    travis

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_BRANCH" = "dev" ]; then
 else
   TAG="$TRAVIS_BRANCH"
 fi
+TRAVIS_REPO_SLUG=`echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]'`
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
