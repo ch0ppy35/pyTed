fbb7b6c3ef0f527de7385627b539ff57a523e7e3
beanie <contact@beaniebot.me>
2020-03-02 15:22:05 -0500

    Remove weekly task from tests

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -86,13 +86,6 @@ class FlaskpyTedTests(unittest.TestCase, xmlunittest.XmlTestMixin):
             minute='59'
         )
         scheduler.add_job(
-            cronTasks.weeklyTasks,
-            trigger='cron',
-            day_of_week='sun',
-            hour='0',
-            minute='0'
-        )
-        scheduler.add_job(
             cronTasks.monthlyTasks,
             trigger='cron',
             day='%(s)s' % {'s': meterRead},
